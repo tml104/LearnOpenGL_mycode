f9984f8b7651d9d1657e0536b7db7c47b5e106d4
TML104 <1041159637@qq.com>
2024-05-13 00:50:05 +0800

    5.1: advanced lighting (blinn phong)

--- 1_4_textures/main.cpp
+++ 1_4_textures/main.cpp
@@ -26,7 +26,7 @@ const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 // 全局变量2：用于相机系统
-Camera camera(glm::vec3(50.0f, 10.0f, 50.0f));
+Camera camera(glm::vec3(10.0f, 10.0f, 10.0f));
 
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
@@ -36,7 +36,11 @@ float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 
 // lighting
-glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+glm::vec3 lightPos(0.0f, 0.0f, 0.0f);
+
+// 全局变量：切换blinn-phong与普通phong（用两个变量，确保在按下B松开后再次按下才会切换状态）
+bool blinn = false;
+bool blinnKeyPressed = false;
 
 int main()
 {
@@ -81,156 +85,45 @@ int main()
     // configure global opengl state
     // -----------------------------
     glEnable(GL_DEPTH_TEST);
-    //glEnable(GL_MULTISAMPLE);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
     // build and compile our shader program
     // ------------------------------------
     
-    Shader antiAliasingShader("./shaders/4_11_AntiAliasing/antiAliasingShader.vs", "./shaders/4_11_AntiAliasing/antiAliasingShader.fs");
-    Shader antiAliasingShader2("./shaders/4_11_AntiAliasing/antiAliasingShader2.vs", "./shaders/4_11_AntiAliasing/antiAliasingShader.fs");
-    Shader antiAliasingPostShader("./shaders/4_11_AntiAliasing/antiAliasingPostShader.vs", "./shaders/4_11_AntiAliasing/antiAliasingPostShader.fs");
+    Shader blinnPhongShader("./shaders/5_1_BlinnPhong/blinnPhongShader.vs", "./shaders/5_1_BlinnPhong/blinnPhongShader.fs");
 
     // load models
-    Model rock("D:/code/learnopengl_resources/rock/rock.obj");
-    Model planet("D:/code/learnopengl_resources/planet/planet.obj");
+    float planeVertices[] = {
+        // positions            // normals         // texcoords
+         10.0f, -0.5f,  10.0f,  0.0f, 1.0f, 0.0f,  10.0f,  0.0f,
+        -10.0f, -0.5f,  10.0f,  0.0f, 1.0f, 0.0f,   0.0f,  0.0f,
+        -10.0f, -0.5f, -10.0f,  0.0f, 1.0f, 0.0f,   0.0f, 10.0f,
 
-    // -> generate a large list of semi-random model transformation matrices
-    auto generate_model_matrices = [&](unsigned int amount) -> glm::mat4* {
-        glm::mat4* modelMatrices = new glm::mat4[amount];
-
-        srand(static_cast<unsigned int>(glfwGetTime())); // initialize random seed
-        float radius = 50.0;
-        float offset = 2.5f;
-        for (unsigned int i = 0; i < amount; i++)
-        {
-            glm::mat4 model = glm::mat4(1.0f);
-            // 1. translation: displace along circle with 'radius' in range [-offset, offset]
-            float angle = (float)i / (float)amount * 360.0f;
-            float displacement = (rand() % (int)(2 * offset * 100)) / 100.0f - offset;
-            float x = sin(angle) * radius + displacement;
-            displacement = (rand() % (int)(2 * offset * 100)) / 100.0f - offset;
-            float y = displacement * 0.4f; // keep height of asteroid field smaller compared to width of x and z
-            displacement = (rand() % (int)(2 * offset * 100)) / 100.0f - offset;
-            float z = cos(angle) * radius + displacement;
-            model = glm::translate(model, glm::vec3(x, y, z));
-
-            // 2. scale: Scale between 0.05 and 0.25f
-            float scale = static_cast<float>((rand() % 20) / 100.0 + 0.05);
-            model = glm::scale(model, glm::vec3(scale));
-
-            // 3. rotation: add random rotation around a (semi)randomly picked rotation axis vector
-            float rotAngle = static_cast<float>((rand() % 360));
-            model = glm::rotate(model, rotAngle, glm::vec3(0.4f, 0.6f, 0.8f));
-
-            // 4. now add to list of matrices
-            modelMatrices[i] = model;
-        }
-
-        return modelMatrices;
+         10.0f, -0.5f,  10.0f,  0.0f, 1.0f, 0.0f,  10.0f,  0.0f,
+        -10.0f, -0.5f, -10.0f,  0.0f, 1.0f, 0.0f,   0.0f, 10.0f,
+         10.0f, -0.5f, -10.0f,  0.0f, 1.0f, 0.0f,  10.0f, 10.0f
     };
-
-    unsigned int amount = 9000;
-    glm::mat4* modelMatrices = generate_model_matrices(amount);
-
-    // -> 为4个顶点属性 (layout (location = 3) mat4) 设置属性指针，并将它们设置为实例化数组
-    unsigned int modelMatricesVBO;
-    glGenBuffers(1, &modelMatricesVBO);
-    glBindBuffer(GL_ARRAY_BUFFER, modelMatricesVBO); // 后面那个mesh循环结束之前别解绑
-    glBufferData(GL_ARRAY_BUFFER, amount * sizeof(glm::mat4), &modelMatrices[0], GL_STATIC_DRAW);
-
-    for (unsigned int i = 0; i < rock.meshes.size(); i++)
-    {
-        unsigned int VAO = rock.meshes[i].VAO;
-        glBindVertexArray(VAO);
-        // 顶点属性
-        GLsizei vec4Size = sizeof(glm::vec4);
-        glEnableVertexAttribArray(3);
-        glVertexAttribPointer(3, 4, GL_FLOAT, GL_FALSE, 4 * vec4Size, (void*)0);
-        glEnableVertexAttribArray(4);
-        glVertexAttribPointer(4, 4, GL_FLOAT, GL_FALSE, 4 * vec4Size, (void*)(vec4Size));
-        glEnableVertexAttribArray(5);
-        glVertexAttribPointer(5, 4, GL_FLOAT, GL_FALSE, 4 * vec4Size, (void*)(2 * vec4Size));
-        glEnableVertexAttribArray(6);
-        glVertexAttribPointer(6, 4, GL_FLOAT, GL_FALSE, 4 * vec4Size, (void*)(3 * vec4Size));
-
-        glVertexAttribDivisor(3, 1);
-        glVertexAttribDivisor(4, 1);
-        glVertexAttribDivisor(5, 1);
-        glVertexAttribDivisor(6, 1);
-
-        glBindVertexArray(0);
-    }
-
-    // -> 屏幕四边形
-    float quadVertices[] = {   // vertex attributes for a quad that fills the entire screen in Normalized Device Coordinates.
-    // positions   // texCoords
-    -1.0f,  1.0f,  0.0f, 1.0f,
-    -1.0f, -1.0f,  0.0f, 0.0f,
-     1.0f, -1.0f,  1.0f, 0.0f,
-
-    -1.0f,  1.0f,  0.0f, 1.0f,
-     1.0f, -1.0f,  1.0f, 0.0f,
-     1.0f,  1.0f,  1.0f, 1.0f
-    };
-
-    // -> 屏幕四边形VAO
-    unsigned int quadVAO, quadVBO;
-    glGenVertexArrays(1, &quadVAO);
-    glGenBuffers(1, &quadVBO);
-    glBindVertexArray(quadVAO);
-    glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+    // plane VAO
+    unsigned int planeVAO, planeVBO;
+    glGenVertexArrays(1, &planeVAO);
+    glGenBuffers(1, &planeVBO);
+    glBindVertexArray(planeVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, planeVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertices), planeVertices, GL_STATIC_DRAW);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glBindVertexArray(0);
 
-    // MSAA Off-screen render framebuffer： MSAA离屏渲染framebuffer
-    unsigned int framebuffer;
-    glGenFramebuffers(1, &framebuffer);
-    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
-
-    // -> 颜色纹理组件（多重采样版）
-    unsigned int textureColorBufferMultiSampled;
-    glGenTextures(1, &textureColorBufferMultiSampled);
-    glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, textureColorBufferMultiSampled);
-    glTexImage2DMultisample(GL_TEXTURE_2D_MULTISAMPLE, 4, GL_RGB, SCR_WIDTH, SCR_HEIGHT, GL_TRUE);  // 第二个参数设置的是纹理所拥有的样本个数。如果最后一个参数为GL_TRUE，图像将会对每个纹素使用相同的样本位置以及相同数量的子采样点个数。
-    glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, 0);
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D_MULTISAMPLE, textureColorBufferMultiSampled, 0); // 颜色附件绑定到当前帧缓冲上
-
-    // -> 渲染缓冲对象（多重采样版）（深度与模版缓冲会使用）
-    unsigned int rbo;
-    glGenRenderbuffers(1, &rbo);
-    glBindRenderbuffer(GL_RENDERBUFFER, rbo);
-    glRenderbufferStorageMultisample(GL_RENDERBUFFER, 4, GL_DEPTH24_STENCIL8, SCR_WIDTH, SCR_HEIGHT); // 渲染缓冲对象后的参数我们将设定为样本的数量，在当前的例子中是4
-    glBindRenderbuffer(GL_RENDERBUFFER, 0);
-    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo); // 渲染缓冲对象附件绑定到当前帧缓冲上
-
-    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
-        cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << endl;
-    glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-    // 后处理帧缓冲 (screenShdaer will be applied)
-    unsigned int intermediateFBO;
-    glGenFramebuffers(1, &intermediateFBO);
-    glBindFramebuffer(GL_FRAMEBUFFER, intermediateFBO);
-
-    // -> 颜色附件
-    unsigned int screenTexture;
-    glGenTextures(1, &screenTexture);
-    glBindTexture(GL_TEXTURE_2D, screenTexture);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, screenTexture, 0);	// we only need a color buffer
-
-    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
-        cout << "ERROR::FRAMEBUFFER:: Intermediate framebuffer is not complete!" << endl;
-    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    unsigned int floorTexture = loadTexture("D:\\code\\vs\\first_glfw\\1_4_textures\\resources\\wood.png");
 
     // 指定 shader 中 纹理采样器所指向的纹理单元（前面的纹理默认绑定到纹理单元0上）
-    antiAliasingPostShader.use();
-    antiAliasingPostShader.setInt("screenTexture", 0);
+    blinnPhongShader.use();
+    blinnPhongShader.setInt("floorTexture", 0);
 
     // render loop
     // -----------
@@ -241,7 +134,6 @@ int main()
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
 
-
         // input
         // -----
         processInput(window);
@@ -251,64 +143,23 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        // 1. 在 MSAA离屏渲染framebuffer 中 渲染场景
-        glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glEnable(GL_DEPTH_TEST);
+        // 渲染：地板
+        blinnPhongShader.use();
 
-        // -> 渲染：中心行星
-        antiAliasingShader.use();
-
-        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 500.0f);
+        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
         glm::mat4 view = camera.GetViewMatrix();
-        antiAliasingShader.setMatrix4("projection", projection);
-        antiAliasingShader.setMatrix4("view", view);
-
         glm::mat4 model = glm::mat4(1.0f);
-        model = glm::translate(model, glm::vec3(0.0f, -3.0f, 0.0f));
-        model = glm::scale(model, glm::vec3(4.0f, 4.0f, 4.0f));
-        antiAliasingShader.setMatrix4("model", model);
-        planet.Draw(antiAliasingShader);
+        model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
+        blinnPhongShader.setMatrix4("projection", projection);
+        blinnPhongShader.setMatrix4("view", view);
+        blinnPhongShader.setMatrix4("model", model);
+        blinnPhongShader.setBool("blinn", blinn);
 
-        // -> 渲染：小行星
-        //for (unsigned int i = 0; i < amount; i++)
-        //{
-        //    instancedAsteroidBeltShader.setMatrix4("model", modelMatrices[i]);
-        //    rock.Draw(instancedAsteroidBeltShader);
-        //}
-
-        antiAliasingShader2.use();
-        antiAliasingShader2.setMatrix4("projection", projection);
-        antiAliasingShader2.setMatrix4("view", view); // 注意：接下来不再手动传入model矩阵了，而是用前面设定的顶点属性3去实现渲染实例时的model矩阵变换
-
-        for (unsigned int i = 0; i < rock.meshes.size(); i++)
-        {
-            glBindVertexArray(rock.meshes[i].VAO);
-            // 这里用glDrawElementsInstanced 是因为在 mesh.h 里面的 draw 也是用的 glDrawElements
-            glDrawElementsInstanced(GL_TRIANGLES, rock.meshes[i].indices.size(), GL_UNSIGNED_INT, 0, amount);
-        }
-
-        // 2. now blit multisampled buffer(s) to normal colorbuffer of intermediate FBO. Image is stored in screenTexture
-        glBindFramebuffer(GL_READ_FRAMEBUFFER, framebuffer); // source
-        glBindFramebuffer(GL_DRAW_FRAMEBUFFER, intermediateFBO); // destination
-        glBlitFramebuffer(
-            0, 0, SCR_WIDTH, SCR_HEIGHT, // src p1, p2
-            0, 0, SCR_WIDTH, SCR_HEIGHT, // des p1, p2
-            GL_COLOR_BUFFER_BIT, GL_NEAREST);
-
-        // 3. 复制完成后，现在可以渲染后处理四边形了（现在intermediateFBO中的screenTexture附件可被着色器采样）
-        glBindFramebuffer(GL_FRAMEBUFFER, 0); // 在默认缓冲中渲染
-        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        glDisable(GL_DEPTH_TEST);
-
-        antiAliasingPostShader.use();
-        glBindVertexArray(quadVAO);
+        glBindVertexArray(planeVAO);
         glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, screenTexture);
+        glBindTexture(GL_TEXTURE_2D, floorTexture);
         glDrawArrays(GL_TRIANGLES, 0, 6);
-
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
@@ -316,7 +167,6 @@ int main()
         glfwPollEvents();
     }
 
-    delete[] modelMatrices;
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
     glfwTerminate();
@@ -358,6 +208,17 @@ void processInput(GLFWwindow* window)
         camera.ProcessMouseMovement(-keyboardMovmentSpeed, 0.0f);
     if (glfwGetKey(window, GLFW_KEY_L) == GLFW_PRESS)
         camera.ProcessMouseMovement(keyboardMovmentSpeed, 0.0f);
+
+    if (glfwGetKey(window, GLFW_KEY_B) == GLFW_PRESS && !blinnKeyPressed) {
+        blinn = !blinn;
+        blinnKeyPressed = true;
+
+        // [debug] blinn
+        std::cout << "blinn: " << blinn << std::endl;
+    }
+    if (glfwGetKey(window, GLFW_KEY_B) == GLFW_RELEASE){
+        blinnKeyPressed = false;
+    }
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
